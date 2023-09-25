b7f4983f086805b57083464b0d88af4a0459d69d
abu usama <usamaliaqat5846@gmail.com>
2023-09-25 14:24:14 +0500

    refactor

--- scripts/build-all-platforms.sh
+++ scripts/build-all-platforms.sh
@@ -27,28 +27,7 @@ fi
 
 for t in                                                                      \
     darwin_amd64                                                              \
-    darwin_arm64                                                              \
-    dragonfly_amd64                                                           \
-    freebsd_386                                                               \
-    freebsd_amd64                                                             \
-    freebsd_arm                                                               \
-    freebsd_arm64                                                             \
-    linux_386                                                                 \
-    linux_amd64                                                               \
-    linux_arm                                                                 \
-    linux_arm64                                                               \
-    netbsd_386                                                                \
-    netbsd_amd64                                                              \
-    netbsd_arm                                                                \
-    netbsd_arm64                                                              \
-    openbsd_386                                                               \
-    openbsd_amd64                                                             \
-    openbsd_arm                                                               \
-    openbsd_arm64                                                             \
-    solaris_amd64                                                             \
-    windows_386                                                               \
-    windows_amd64                                                             \
-    windows_arm ;
+    linux_amd64 ;
 do
     os="${t%_*}"
     arch="${t#*_}"
